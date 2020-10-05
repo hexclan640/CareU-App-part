c58094fc431025e3fb16545158d12b86e97cd41f
LManjitha <lmanjithah@gmail.com>
2020-10-05 22:37:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/careu/loginPage.java
@@ -0,0 +1,28 @@
+package com.example.careu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class loginPage extends AppCompatActivity {
+
+    Button btnlogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        btnlogin = findViewById(R.id.btnLog);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),homePage.class);
+                startActivity(i);
+            }
+        });
+    }
+}
